56c6df989235a68436f08441c7651974519c5d0a
Filipe Fernandes <ocefpaf@gmail.com>
2017-12-08 14:43:40 -0200

    test issue 31

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -3,3 +3,8 @@ import geopandas
 
 fname = os.path.join(os.environ['RECIPE_DIR'], 'test_data', 'test.shp')
 geopandas.read_file(fname)
+
+
+import fiona
+div20m = geopandas.read_file('test_issue_31')
+print(div20m.head())
